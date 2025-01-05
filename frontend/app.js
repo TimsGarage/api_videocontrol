a6200306b9ab533450743d6d7af63e1e7c18fb04
TimSalokat <Tim.salokat@outlook.com>
2025-01-05 15:17:23 +0100

    Added handler for all buttons

--- frontend/app.js
+++ frontend/app.js
@@ -13,11 +13,31 @@ let sendToServer = function(target, message) {
     });
 }
 
-document.getElementById('beamer-play').addEventListener('click', () => sendToServer("beamer", "play"));
+// Beamer
+document.getElementById('beamer-rewind').addEventListener('click', () => sendToServer("beamer", "rewind"));
 document.getElementById('beamer-pause').addEventListener('click', () => sendToServer("beamer", "pause"));
-document.getElementById('beamer-mute').addEventListener('click', () => sendToServer("beamer", "mute"));
+document.getElementById('beamer-play').addEventListener('click', () => sendToServer("beamer", "play"));
+document.getElementById('beamer-skip').addEventListener('click', () => sendToServer("beamer", "skip"));
 
-document.getElementById('laptop-play').addEventListener('click', () => sendToServer("laptop", "play"));
+document.getElementById('beamer-volDown').addEventListener('click', () => sendToServer("beamer", "volDown"));
+document.getElementById('beamer-mute').addEventListener('click', () => sendToServer("beamer", "mute"));
+document.getElementById('beamer-unmute').addEventListener('click', () => sendToServer("beamer", "unmute"));
+document.getElementById('beamer-volUp').addEventListener('click', () => sendToServer("beamer", "volUp"));
+
+document.getElementById('beamer-fullscreen').addEventListener('click', () => sendToServer("beamer", "fullscreen"));
+document.getElementById('beamer-smallscreen').addEventListener('click', () => sendToServer("beamer", "smallscreen"));
+
+// Laptop
+document.getElementById('laptop-rewind').addEventListener('click', () => sendToServer("laptop", "rewind"));
 document.getElementById('laptop-pause').addEventListener('click', () => sendToServer("laptop", "pause"));
+document.getElementById('laptop-play').addEventListener('click', () => sendToServer("laptop", "play"));
+document.getElementById('laptop-skip').addEventListener('click', () => sendToServer("laptop", "skip"));
+
+document.getElementById('laptop-volDown').addEventListener('click', () => sendToServer("laptop", "volDown"));
 document.getElementById('laptop-mute').addEventListener('click', () => sendToServer("laptop", "mute"));
+document.getElementById('laptop-unmute').addEventListener('click', () => sendToServer("laptop", "unmute"));
+document.getElementById('laptop-volUp').addEventListener('click', () => sendToServer("laptop", "volUp"));
+
+document.getElementById('laptop-fullscreen').addEventListener('click', () => sendToServer("laptop", "fullscreen"));
+document.getElementById('laptop-smallscreen').addEventListener('click', () => sendToServer("laptop", "smallscreen"));
 
